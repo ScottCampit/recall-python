8f192c179592733f485d23bed0ec524615efb4c0
Scott Campit <scott@couro.io>
2025-03-26 11:16:57 -0700

    Remove unnecessary scripts

--- scripts/run_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t recall-app .
-docker run -it --rm -p 8501:8501 -v $(pwd)/.env:/app/.env recall-app
-    -p 8501:8501 \
-    -v $(pwd)/.env:/app/.env \
-    recall-app 
